94ec25960e413b99682d88277f41a84e93c8cbac
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-11-01 11:01:27 -0400

    chore: update eslint config to support node globals

--- packages/eslint-config/index.mjs
+++ packages/eslint-config/index.mjs
@@ -1,11 +1,20 @@
 import js from "@eslint/js";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from "globals";
 
 export default [
   js.configs.recommended,
   {
     name: "base/ignores",
     ignores: ["node_modules/**", "dist/**"],
+  },
+  {
+    name: "base/globals",
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
   },
   {
     name: "base/unused-disable-directives",
