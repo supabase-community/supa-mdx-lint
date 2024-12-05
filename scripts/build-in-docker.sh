2979695e641b22278f51a9d93e352f83dd2b7cfa
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-12-05 16:19:38 -0500

    ci(build): build for linux

---
+++ scripts/build-in-docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Adapted from https://github.com/getsentry/sentry-cli
+# See original license: https://github.com/getsentry/sentry-cli/blob/master/LICENSE
+
+set -eux
+
+DOCKER_IMAGE="messense/rust-musl-cross:${DOCKER_TAG}"
+BUILD_DIR="/work"
+
+DOCKER_RUN_OPTS="
+  -w ${BUILD_DIR}
+  -v $(pwd):${BUILD_DIR}:ro
+  -v $(pwd)/target:${BUILD_DIR}/target
+  -v $HOME/.cargo/registry:/root/.cargo/registry
+  ${DOCKER_IMAGE}
+"
+
+docker run \
+  ${DOCKER_RUN_OPTS} \
+  cargo build --release --target=${TARGET} --locked
+
+# Fix permissions for shared directories
+USER_ID=$(id -u)
+GROUP_ID=$(id -g)
+sudo chown -R ${USER_ID}:${GROUP_ID} target/ $HOME/.cargo
